8fa748b1efa2cc2120c3cca235f560dc3daf4b02
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-11-15 22:08:26 +0100

    made the appendonlystr thread safe

--- append_only_str/src/lib.rs
+++ append_only_str/src/lib.rs
@@ -1,17 +1,28 @@
-use core::fmt;
-use std::cell::Cell;
+use std::{
+    ptr, str,
+    sync::{Arc, RwLock},
+};
 
 use rawbuf::RawBuf;
 mod rawbuf;
 
+/// A thread safe append only string
 pub struct AppendOnlyStr {
-    data: RawBuf,
+    // The reason there is an arc here is that
+    // it allows for references to be kept alive
+    // even though the buffer has been reallocated
+    // which would invalidate those references
+    rawbuf: Arc<RawBuf>,
+    // `len` doesn't need to be an RwLock because
+    // the only time len is modified we have exclusive
+    // access to it
+    len:usize,
 }
 
 impl std::fmt::Debug for AppendOnlyStr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("AppendOnlyStr")
-            .field("data", &self.data.get_str())
+            .field("data", &self.get_str())
             .finish_non_exhaustive()
     }
 }
@@ -25,13 +36,15 @@ impl Default for AppendOnlyStr {
 impl AppendOnlyStr {
     pub fn new() -> Self {
         Self {
-            data: RawBuf::new(),
+            rawbuf: Arc::new(RawBuf::new()),
+            len: 0.into(),
         }
     }
 
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
-            data: RawBuf::with_capacity(capacity),
+            rawbuf: Arc::new(RawBuf::with_capacity(capacity)),
+            len: 0.into(),
         }
     }
 
@@ -40,5 +53,76 @@ impl AppendOnlyStr {
         c.encode_utf8(&mut buf);
     }
 
-    pub fn push_str(c: &str) {}
+    /// Guarantees that the buffer will be
+    /// able to hold n more bytes
+    ///
+    /// # Panics
+    /// The function panics if the new capacity overflows
+    pub fn reserve(&mut self, amount: usize) {
+        let len = *self.len.read().unwrap();
+        let target = len + amount;
+        if target <= self.rawbuf.capacity() {
+            // We have space for the reservation
+            return;
+        }
+        let mut new_capacity = self.rawbuf.capacity();
+        while target > new_capacity {
+            match new_capacity.checked_mul(2) {
+                Some(x) => new_capacity = x,
+                // This ensures that the new buffer is at least as large as
+                // it is supposed to be. This probably isn't the best handling,
+                // but this is an edgecase anyways
+                None => new_capacity = target,
+            }
+        }
+
+        let original = std::mem::replace(self, Self::with_capacity(new_capacity));
+        //// SAFETY: -----------------------------
+        //// The two buffers are non-overlapping, and are both at least
+        //// `original.rawbuf.capacity()` long
+        unsafe {
+            std::ptr::copy_nonoverlapping(
+                original.rawbuf.ptr(),
+                self.rawbuf.ptr(),
+                original.rawbuf.capacity(),
+            );
+        }
+    }
+
+    unsafe fn write_unchecked(&mut self, bytes: &[u8]) {
+        std::ptr::copy(bytes.as_ptr(), self.rawbuf.ptr(), bytes.len());
+        *self.len.write().unwrap() += bytes.len();
+    }
+
+    /// # Safety
+    /// This assumes that the bytes are utf-8 compliant
+    pub unsafe fn push_bytes(&mut self, bytes: &[u8]) {
+        self.reserve(bytes.len());
+        self.write_unchecked(bytes);
+    }
+
+    pub fn push_str(&mut self, str: &str) {
+        unsafe { self.push_bytes(str.as_bytes()) }
+    }
+
+    pub fn get_str(&self) -> &str {
+        //// SAFETY: ---------------------------------------------
+        //// We never make the capacity greater than the amount of
+        //// space allocated. and therefore a slice won't read
+        //// uninitialized memory
+        str::from_utf8(unsafe {
+            std::ptr::slice_from_raw_parts(
+                self.rawbuf.ptr().cast_const(),
+                self.len,
+            )
+            .as_ref()
+            .unwrap()
+        })
+        .unwrap()
+    }
 }
+
+/// SAFETY: AppendOnlyStr does not allow for interior mutability
+/// without exclusive access and is therefore `Sync` & `Send`
+unsafe impl Sync for AppendOnlyStr {}
+unsafe impl Send for AppendOnlyStr {}
--- append_only_str/src/rawbuf.rs
+++ append_only_str/src/rawbuf.rs
@@ -7,7 +7,6 @@ use std::{
 pub(crate) struct RawBuf {
     ptr: NonNull<u8>,
     capacity: usize,
-    len: usize,
 }
 
 impl RawBuf {
@@ -15,7 +14,6 @@ impl RawBuf {
         Self {
             ptr: NonNull::dangling(),
             capacity: 0,
-            len: 0,
         }
     }
 
@@ -34,74 +32,31 @@ impl RawBuf {
             Some(p) => p,
             None => alloc::handle_alloc_error(layout),
         };
-        Self {
-            ptr,
-            capacity,
-            len: 0,
-        }
+        Self { ptr, capacity }
+    }
+
+    pub unsafe fn write_unchecked(&mut self, bytes: &[u8]) {
+        ptr::copy(bytes.as_ptr(), self.ptr.as_ptr(), bytes.len());
     }
 
     #[inline(always)]
-    pub fn len(&self) -> usize {
-        self.len
+    pub fn capacity(&self) -> usize {
+        self.capacity
     }
 
-    /// Guarantees that the buffer will be
-    /// able to hold n more bytes
-    ///
-    /// # Panics
-    /// The function panics if the new capacity overflows
-    pub fn reserve(&mut self, amount: usize) {
-        let target = self.len + amount;
-        if target <= self.capacity {
-            // We have space for the reservation
-            return;
-        }
-        let mut new_capacity = self.capacity;
-        while target > new_capacity {
-            match new_capacity.checked_mul(2) {
-                Some(x) => new_capacity = x,
-                // This ensures that the new buffer is at least as large as
-                // it is supposed to be. This probably isn't the best handling,
-                // but this is an edgecase anyways
-                None => new_capacity = target,
-            }
-        }
+    #[inline(always)]
+    pub fn ptr(&self) -> *mut u8 {
+        self.ptr.as_ptr()
+    }
+}
 
-        let new_buffer = Self::with_capacity(new_capacity);
-        //// SAFETY: -----------------------------
-        //// `new_buffer` is a new buffer and can therefore not overlap
-        //// with the old buffer.
-        //// The target destination has enough space because `new_capacity`
-        //// is strictly bigger than `self.capacity`
+impl Drop for RawBuf {
+    fn drop(&mut self) {
         unsafe {
-            std::ptr::copy_nonoverlapping(
+            alloc::dealloc(
                 self.ptr.as_ptr(),
-                new_buffer.ptr.as_ptr(),
-                self.capacity,
-            );
+                Layout::array::<u8>(self.capacity).unwrap(),
+            )
         }
-
-        *self = new_buffer;
-    }
-
-    /// # SAFETY
-    /// This assumes that the bytes are utf-8 compliant
-    pub unsafe fn push_bytes(&mut self, bytes: &[u8]) {
-        self.reserve(bytes.len());
-        self::ptr::copy(bytes.as_ptr(), self.ptr.as_ptr(), bytes.len());
-    }
-
-    pub fn push_str(&mut self, str: &str) {
-        unsafe { self.push_bytes(str.as_bytes()) }
-    }
-
-    pub fn get_str(&self) -> &str {
-        str::from_utf8(unsafe {
-            std::ptr::slice_from_raw_parts(self.ptr.as_ptr().cast_const(), self.len)
-                .as_ref()
-                .unwrap()
-        })
-        .unwrap()
     }
 }
