18b891c01688882927a2cd2891f7f87cf94b9350
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-11-16 09:22:07 +0100

    fixed off-by-one

--- piece-table/src/iters.rs
+++ piece-table/src/iters.rs
@@ -1,4 +1,3 @@
-use core::slice;
 use std::{iter, ops::RangeInclusive, str};
 
 use append_only_str::AppendOnlyStr;
@@ -28,12 +27,12 @@ where
                 self.main
                     .chars()
                     .skip(*current_range.start())
-                    .take(*current_range.end() - *current_range.start())
+                    .take(*current_range.end() - *current_range.start() + 1)
             } else {
                 self.clients[buf - 1]
                     .chars()
                     .skip(*current_range.start())
-                    .take(*current_range.end() - *current_range.start())
+                    .take(*current_range.end() - *current_range.start() + 1)
             })
         }
         if let Some(x) = self.current_iter.as_mut().unwrap().next() {
@@ -102,6 +101,24 @@ mod test {
     use crate::{Buffers, Piece, PieceTable};
 
     #[test]
+    fn test_chars_no_clients() {
+        let text = "test\nmore tests\n";
+        let piece = Piece {
+            buffers: Buffers {
+                original: text.to_string().into_boxed_str(),
+                clients: vec![],
+            },
+            piece_table: PieceTable {
+                table: LinkedList::from_iter(std::iter::once((0, 0..=text.len() - 1))),
+                cursors: vec![],
+            },
+        };
+        let mut chars = piece.chars();
+        assert_eq!(chars.next(), Some('t'));
+        assert_eq!(chars.next(), Some('e'));
+    }
+
+    #[test]
     fn test_lines_no_clients() {
         let text = "test\nmore tests\n";
         let piece = Piece {
@@ -117,11 +134,13 @@ mod test {
 
         let mut lines = piece.lines();
         assert_eq!(lines.next(), Some("test".to_string()));
+        assert_eq!(lines.next(), Some("more tests".to_string()));
+        assert_eq!(lines.next(), None);
     }
 
     #[test]
-    fn test_chars_no_clients() {
-        let text = "test\nmore tests\n";
+    fn test_trailing_no_clients() {
+        let text = "test\nmore tests\na";
         let piece = Piece {
             buffers: Buffers {
                 original: text.to_string().into_boxed_str(),
@@ -132,10 +151,11 @@ mod test {
                 cursors: vec![],
             },
         };
-        let test = piece.piece_table.table.iter();
-        let test = piece.piece_table.table.iter();
-        let mut chars = piece.chars();
-        assert_eq!(chars.next(), Some('t'));
-        assert_eq!(chars.next(), Some('e'));
+
+        let mut lines = piece.lines();
+        assert_eq!(lines.next(), Some("test".to_string()));
+        assert_eq!(lines.next(), Some("more tests".to_string()));
+        assert_eq!(lines.next(), Some("a".to_string()));
+        assert_eq!(lines.next(), None);
     }
 }
