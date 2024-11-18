db83857126a5e76cc96fcee6728795c066cbc77b
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2024-11-18 08:30:53 +0100

    added fromiter for appendonlystr

--- append_only_str/src/iters.rs
+++ append_only_str/src/iters.rs
@@ -1,7 +1,7 @@
 //! Implements iterator helper functions for `AppendOnlyStr`
 
 use crate::AppendOnlyStr;
-use std::str;
+use std::str::{self, FromStr};
 
 impl AppendOnlyStr {
     /// Returns an iterator over all chars
@@ -10,5 +10,15 @@ impl AppendOnlyStr {
     /// on the original `AppendOnlyStr`
     pub fn chars(&self) -> str::Chars {
         self.get_str().chars()
+    }
+}
+
+impl FromIterator<u8> for AppendOnlyStr {
+    fn from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = u8>,
+    {
+        Self::from_str(str::from_utf8(iter.into_iter().collect::<Vec<_>>().as_slice()).unwrap())
+            .unwrap()
     }
 }
